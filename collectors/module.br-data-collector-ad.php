da088ac30661658293dd44762c4876ce4bab5bf9
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-11-25 16:58:00 +0100

    set version to 0.0.1

--- collectors/module.br-data-collector-ad.php
+++ collectors/module.br-data-collector-ad.php
@@ -2,7 +2,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'br-data-collector-ad/1.1.0',
+	'br-data-collector-ad/0.0.1',
 	array(
 		// Identification
 		//
