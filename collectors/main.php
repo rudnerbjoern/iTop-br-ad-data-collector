06566508f98cc0d08285fcc9ba8c7d14135f2bdc
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-11-26 17:28:09 +0100

    change directory structure

--- collectors/main.php
+++ collectors/main.php
@@ -1,6 +1,7 @@
 <?php
-require_once(APPROOT . 'collectors/LDAPCollector.class.inc.php');
-require_once(APPROOT . 'collectors/iTopPCLDAPCollector.class.inc.php');
+
+require_once(APPROOT . 'collectors/src/LDAPCollector.class.inc.php');
+require_once(APPROOT . 'collectors/src/iTopPCLDAPCollector.class.inc.php');
 
 Orchestrator::AddRequirement('1.0.0', 'ldap'); // LDAP support is required to run this collector
 
